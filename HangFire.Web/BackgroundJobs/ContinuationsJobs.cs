32cd6e5832f2e23f48a42fdc74e4f40490cd7734
t4h4 <tahayasin@outlook.com>
2021-03-30 18:06:55 +0300

    Continuations jobs eklendi. (Bir job sonrası ardına çalışan jobs.)

---
+++ HangFire.Web/BackgroundJobs/ContinuationsJobs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HangFire.Web.BackgroundJobs
+{
+    public class ContinuationsJobs
+    {
+        public static void WriteWatermarkStatusJob(string id, string fileName) //job id'ye ihtiyaç var. hangi job'tan sonra çalışması gerektiğini bilmesi için.
+        {
+            Hangfire.BackgroundJob.ContinueJobWith(id, () => Debug.WriteLine($"{fileName} : resim'e watermark eklenmiştir."));
+        }
+    }
+}
--- HangFire.Web/Controllers/HomeController.cs
+++ HangFire.Web/Controllers/HomeController.cs
@@ -69,6 +69,7 @@ namespace HangFire.Web.Controllers
                 }
 
                 string jobID = BackgroundJobs.DelayedJobs.AddWaterMarkJob(newFileName, "TAHA");
+                BackgroundJobs.ContinuationsJobs.WriteWatermarkStatusJob(jobID, newFileName);
             }
 
             return View();
